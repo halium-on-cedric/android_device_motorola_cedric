542ea807539742950d4b8cba7ccaaec0123ba6dc
Hacker1245 <29224894+Hacker1245@users.noreply.github.com>
2020-01-24 18:13:37 +0100

    copy udev rules

--- device.mk
+++ device.mk
@@ -263,7 +263,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf \
     p2p_supplicant_overlay.conf \
     wpa_supplicant_overlay.conf
-
+    
+# Udev rules
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/ubuntu/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules \
+    
 PRODUCT_PACKAGES += \
     libcurl \
     libqsap_sdk \
